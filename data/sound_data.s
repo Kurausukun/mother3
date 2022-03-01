440a4338ab4d8df153d2cd66510d90cdde747e08
Kurausukun <lord.uber1@gmail.com>
2022-02-28 20:10:44 -0500

    split m4a data

---
+++ data/sound_data.s
@@ -0,0 +1,12 @@
+	.include "asm/macros/m4a.inc"
+	.include "asm/macros/music_voice.inc"
+
+	.section .rodata
+	
+	.include "sound/voice_groups.inc"
+	.include "sound/keysplit_tables.inc"
+	.include "sound/programmable_wave_data.inc"
+	.include "sound/music_player_table.inc"
+	.include "sound/song_table.inc"
+	.include "sound/direct_sound_data.inc"
+	.include "sound/song_data.inc"
