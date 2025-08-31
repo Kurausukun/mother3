49d0676803d73de5e415c6d92557c19dd1396de2
Theo Hytopoulos <theo@Theos-MacBook-Pro.local>
2025-08-31 11:35:50 -0700

    use docker for building

---
+++ setup.sh
@@ -0,0 +1,6 @@
+docker run --platform=linux/amd64 --rm -it \
+  -v "$PWD":/work -w /work \
+  -e DEVKITPRO=/opt/devkitpro \
+  -e DEVKITARM=/opt/devkitpro/devkitARM \
+  devkitpro/devkitarm:latest \
+  bash -lc 'make setup'
