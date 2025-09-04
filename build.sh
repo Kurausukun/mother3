bdb1507569a806ba6e3eae03ea7e1aa82207dac0
Theo Hytopoulos <theo@Theos-MacBook-Pro.local>
2025-09-04 09:59:36 -0700

    suppress build output for better debugigng

--- build.sh
+++ build.sh
@@ -3,8 +3,4 @@ docker run --platform=linux/amd64 --rm -it \
   -e DEVKITPRO=/opt/devkitpro \
   -e DEVKITARM=/opt/devkitpro/devkitARM \
   devkitpro/devkitarm:latest \
-<<<<<<< Updated upstream
-  bash -lc "make $@"
-=======
-  bash -lc 'make -s'
->>>>>>> Stashed changes
+  bash -lc "make -s $@"
