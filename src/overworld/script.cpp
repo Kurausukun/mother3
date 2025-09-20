66f001a71450b1cfeaca007592539403e0aa112e
StableCrimson <rhysethompson@gmail.com>
2025-09-20 17:18:19 -0500

    Match cmd_B1

--- src/overworld/script.cpp
+++ src/overworld/script.cpp
@@ -4766,7 +4766,11 @@ extern "C" s32 cmd_B0() {
     return 0;
 }
 
-extern "C" ASM_FUNC("asm/non_matching/script/cmd_B1.inc", void cmd_B1());
+extern "C" s32 cmd_B1() {
+    scriptstack_push(gGame._82b9[0xBA]);
+    return 0;
+}
+
 extern "C" ASM_FUNC("asm/non_matching/script/sub_08021878.inc", u8* sub_08021878(void* r0, s32* r1, u16* r2));
 extern "C" ASM_FUNC("asm/non_matching/script/sub_080218B0.inc", void sub_080218B0(u32 r0, u32 r1));
 
