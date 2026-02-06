33c1c4cf40d448b2e980a7439cae64f455f4e052
StableCrimson <rhysethompson@gmail.com>
2026-02-05 20:16:48 -0600

    Match initDebugMenuPage4

--- src/code_08021920.cpp
+++ src/code_08021920.cpp
@@ -1334,7 +1334,15 @@ extern "C" void initDebugMenuPage3(TransactionState* state) {
     gSomeBlend._360e_2 = 1;
 }
 
-extern "C" ASM_FUNC("asm/non_matching/code_08021920/initDebugMenuPage4.inc", void initDebugMenuPage4());
+extern "C" void initDebugMenuPage4(TransactionState* state) {
+    gSomeBlend.currentDebugPage = 3;
+    state->tabIndex = 0;
+    state->transactionType = 0;
+    state->numTabs = 3;
+    gSomeBlend._360e_1 = 1;
+    gSomeBlend._360e_2 = 1;
+}
+
 extern "C" ASM_FUNC("asm/non_matching/code_08021920/initDebugMenuPage5.inc", void initDebugMenuPage5());
 extern "C" ASM_FUNC("asm/non_matching/code_08021920/initDebugMenuPage6.inc", void initDebugMenuPage6());
 
