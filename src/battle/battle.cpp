70cc4754f4838ba2aa9ec49fb57e9263d0d40de5
wunnr <wunner@protonmail.com>
2026-05-05 22:44:07 -0600

    Resolve Round/UnitTurn func names

--- src/battle/battle.cpp
+++ src/battle/battle.cpp
@@ -482,15 +482,15 @@ extern "C" void nullsub_95() {}
 extern "C" ASM_FUNC("asm/non_matching/battle/sub_0805EDD8.inc", void _GLOBAL_I_BattleManager()); // BattleManager_global_init
 extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EE80.inc", void __6Battle()); // __6Battle
 
-extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EEA0.inc", void sub_0805EEA0());
-extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EEA4.inc", void sub_0805EEA4());
-extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EEC0.inc", void sub_0805EEC0());
-extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EED8.inc", void sub_0805EED8());
-extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EEDC.inc", void sub_0805EEDC());
-extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EEF8.inc", void sub_0805EEF8());
-extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EF10.inc", void sub_0805EF10());
-extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EF14.inc", void sub_0805EF14());
-extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EF34.inc", void sub_0805EF34());
-extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EF50.inc", void sub_0805EF50());
-extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EF54.inc", void sub_0805EF54());
-extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EF74.inc", void sub_0805EF74()); // RoundBegin::RoundBegin() : t(0) {}
+extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EEA0.inc", void u__11UnitTurnEnd()); // UnitTurnEnd::u() { return u; }
+extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EEA4.inc", void __11UnitTurnEndP4Unit()); // UnitTurnEnd::UnitTurnEnd(Unit* u) : u(u) {}
+extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EEC0.inc", void __11UnitTurnEnd()); // UnitTurnEnd::UnitTurnEnd() : u(0) {}
+extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EED8.inc", void u__13UnitTurnBegin()); // UnitTurnBegin::u() { return u; }
+extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EEDC.inc", void __13UnitTurnBeginP4Unit()); // UnitTurnBegin::UnitTurnBegin(Unit* u) : u(u) {}
+extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EEF8.inc", void __13UnitTurnBegin()); // UnitTurnBegin::UnitTurnBegin() : u(0) {}
+extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EF10.inc", void t___8RoundEnd()); // RoundEnd::t() { return t; }
+extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EF14.inc", void __8RoundEndUs()); // RoundEnd::RoundEnd(u16) : t(t) {}
+extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EF34.inc", void __8RoundEnd()); // RoundEnd::RoundEnd() : t(0) {}
+extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EF50.inc", void t__10RoundBegin()); // RoundBegin::t() { return t; }
+extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EF54.inc", void __10RoundBeginUs()); // RoundBegin::RoundBegin(u16) : t(t) {}
+extern "C" ASM_FUNC("asm/non_matching/guest/sub_0805EF74.inc", void __10RoundBegin()); // RoundBegin::RoundBegin() : t(0) {}
